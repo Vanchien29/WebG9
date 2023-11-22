665a80c86e284b8bbdc9f6aa7eb51ac41ccf846e
Cuong <cuongcaotien9a@gmail.com>
2023-11-22 15:25:08 +0700

    update

--- javascript/sanpham_ad.js
+++ javascript/sanpham_ad.js
@@ -1,50 +1,35 @@
-function loadItem() {
-    let beginIndex = limit * (thisPage - 1);
-    let endIndex = limit * thisPage - 1;
-    for (let i = 0; i < list.length; i++) {
-      if (i >= beginIndex && i <= endIndex) list[i].style.display = "block";
-      else list[i].style.display = "none";
-    }
-    listPage();
+function toggleDisplay(){
+  let elements = document.getElementsByClassName('item');
+  for (let i = 0; i < elements.length; i++) {
+    // Toggle giữa 'block' và 'none'
+    elements[i].style.display = (elements[i].style.display === 'block') ? 'none' : 'block';
   }
 
-function listPage() {
-    let count = Math.ceil(list.length / limit);
-    document.querySelector(".list-page").innerHTML = "";
-    if (thisPage != 1) {
-      let prev = document.createElement("li");
-      prev.innerText = "Trước";
-      prev.setAttribute("onclick", "changePage(" + (thisPage - 1) + ")");
-      document.querySelector(".list-page").appendChild(prev);
-    }
-    for (let i = 1; i <= count; i++) {
-      let newPage = document.createElement("li");
-      newPage.innerText = i;
-      if (i == thisPage) {
-        newPage.classList.add("page-current");
-      }
-      newPage.setAttribute("onclick", "changePage(" + i + ")");
-      document.querySelector(".list-page").appendChild(newPage);
-    }
-    if (thisPage != count) {
-      let next = document.createElement("li");
-      next.innerText = "Sau";
-      next.setAttribute("onclick", "changePage(" + (thisPage + 1) + ")");
-      document.querySelector(".list-page").appendChild(next);
-    }
+  let previousBtn = document.getElementById('previousBtn');
+  previousBtn.style.display = (previousBtn.style.display === 'none') ? 'inline-block' : 'none';
+
+  let nextBtn = document.getElementById('nextBtn');
+  nextBtn.style.display = (nextBtn.style.display === 'none') ? 'inline-block' : 'none';
+}
+
+function kickQuaTrang(pageNumber){
+  let page1 = document.getElementById('page1');
+  let page2 = document.getElementById('page2');
+  if(pageNumber == 1){
+    page1.style.backgroundColor = 'black';
+    page1.style.color = 'white';
   }
-  function changePage(i) {
-    thisPage = i;
-    loadItem();
+  else {
+    page1.style.backgroundColor = '#EFEFEF';
+    page1.style.color = 'black'; 
   }
-  function price_format(price) {
-    if (price == "") return "";
-    let price_str = "";
-    let tmp = price;
-    for (i = price.length; i > 3; i -= 3) {
-      price_str = "." + tmp.slice(-3) + price_str;
-      tmp = tmp.substr(0, i - 3);
-    }
-    tmp = tmp.slice(0);
-    return tmp + price_str + "₫";
+
+  if(pageNumber == 2){
+    page2.style.backgroundColor = 'black';
+    page2.style.color = 'white';
   }
+  else {
+    page2.style.backgroundColor = '#EFEFEF';
+    page2.style.color = 'black';
+  }
+}
